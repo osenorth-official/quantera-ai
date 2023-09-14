c6b0de6afb30c6aa3a47aedf06a15b6799aa6d75
Aditya Singh <adity.347317@gmail.com>
2023-09-14 21:19:14 +0530

    Finished section 7

---
+++ components/customAccordion.tsx
@@ -0,0 +1,31 @@
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+
+export default function CustomAccordion(props: any) {
+  return (
+    <Accordion
+      sx={{ bgcolor: "#F5F6F6", boxShadow: "none", maxWidth: "80%", mt: 2 }}
+      expanded={props.expanded}
+    >
+      <AccordionSummary
+        expandIcon={<AddIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography>{props.question}</Typography>
+      </AccordionSummary>
+      <AccordionDetails sx={{ mr: 10 }}>
+        <Typography>
+          Of course, we are always happy to hear from people who have ideas!
+          Send them along to <br />
+          <a href="mailto:contact@quantera.com">contact@quantera.com</a>
+        </Typography>
+      </AccordionDetails>
+    </Accordion>
+  );
+}
--- sections/pre_live_home_sections/section7.tsx
+++ sections/pre_live_home_sections/section7.tsx
@@ -1,4 +1,14 @@
-import { Grid, Typography } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  Button,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import AddIcon from "@mui/icons-material/Add";
+import CustomAccordion from "@/components/customAccordion";
 
 export default function Section7() {
   return (
@@ -10,13 +20,39 @@ export default function Section7() {
       </Grid>
       <Grid item xs={12} sx={{ mt: 5 }}>
         <Typography variant="body1" align="center">
-          Student must be enrolled in a university program and <br /> be able to show proof of enrolment.
+          Student must be enrolled in a university program and <br /> be able to
+          show proof of enrolment.
         </Typography>
       </Grid>
-      <Grid item xs={12} sx={{ mt: 5 }}>
-        <Typography variant="body1" align="center">
-          Frequently asked questions?
-        </Typography>
+      <Grid item xs={12} sx={{ mt: 5, ml: "45%" }}>
+        <Button
+          variant="contained"
+          style={{
+            backgroundColor: "#73F3C4",
+            color: "black",
+            textTransform: "none",
+          }}
+          endIcon={<ArrowForwardIcon />}
+        >
+          Get in touch
+        </Button>
+      </Grid>
+      <Grid container item xs={12} sx={{ mt: 7, ml: 20 }}>
+        <Grid item xs={6}>
+          <Typography variant="h4" align="left">
+            Frequently asked questions?
+          </Typography>
+          <Typography variant="body1" align="left" sx={{ mt: 2 }}>
+            We assembled some of the most common <br /> questions
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <CustomAccordion question="Can I cancel my subscription anytime?" expanded={false}/>
+          <CustomAccordion question="Do you use my data for anything?" expanded={false}/>
+          <CustomAccordion question="I would like to request a feature!" expanded={true}/>
+          <CustomAccordion question="When does my discount apply?" expanded={false}/>
+          
+        </Grid>
       </Grid>
     </Grid>
   );

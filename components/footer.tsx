9479837b0a8082a027261b84274111d3c7eb7b3d
Aditya Singh <adity.347317@gmail.com>
2023-09-18 17:48:32 +0530

    Added supabase

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState } from "react";
 import {
   Grid,
@@ -11,9 +12,26 @@ import {
 import Image from "next/image";
 import CopyrightIcon from "@mui/icons-material/Copyright";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export default function Footer() {
+  const supabase = createClientComponentClient();
   const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState(false);
+
+  const onGetPremiumReport = async () => {
+    setEmailError(false);
+    if (!email.match(/.+@.+/)) {
+      setEmailError(true);
+      return;
+    }
+    await supabase.from("email").insert([
+      {
+        email: email,
+      },
+    ]);
+    setEmail("");
+  };
   return (
     <Grid
       container
@@ -24,12 +42,16 @@ export default function Footer() {
         isplay: "flex",
         minHeight: "35vh",
         minWidth: "100vw",
-        p: 11 
+        p: 11,
       }}
     >
       <Grid item xs={5} sx={{ ml: 10 }}>
         <Image src="/Logo.png" alt="banner" height={60} width={240} />
-        <Typography variant="body1" style={{ color: "white", fontSize:"1.25rem", fontWeight:600 }} align="left">
+        <Typography
+          variant="body1"
+          style={{ color: "white", fontSize: "1.25rem", fontWeight: 600 }}
+          align="left"
+        >
           Data visualisation and stock insights have
           <br />
           taken a back seat. We bring data back to
@@ -43,21 +65,38 @@ export default function Footer() {
             <LinkedInIcon style={{ color: "white", fontSize: 35 }} />
           </IconButton>
         </Stack>
-        <Typography variant="body1" style={{ color: "white", fontSize:"0.875rem", fontWeight:600 }} align="left">
+        <Typography
+          variant="body1"
+          style={{ color: "white", fontSize: "0.875rem", fontWeight: 600 }}
+          align="left"
+        >
           <CopyrightIcon /> {new Date().getFullYear()} Quantera AI. All rights
           reserved.
         </Typography>
       </Grid>
       <Grid item xs={6} sx={{ mt: 5 }}>
-        <Typography variant="h5" style={{ color: "white", fontSize:"2rem", fontWeight:900 }} align="right">
+        <Typography
+          variant="h5"
+          style={{ color: "white", fontSize: "2rem", fontWeight: 900 }}
+          align="right"
+        >
           Recieve a free premium report by signing up
         </Typography>
         <TextField
           label="Email"
           variant="outlined"
-          sx={{ width: "30vw", p: 0, bgcolor: 'white', borderRadius: 2, ml:"32%", mt: 2 }}
+          sx={{
+            width: "30vw",
+            p: 0,
+            bgcolor: "white",
+            borderRadius: 2,
+            ml: "32%",
+            mt: 2,
+          }}
           value={email}
           size="small"
+          error={emailError}
+          helperText={emailError ? "Please enter a valid email." : ""}
           onChange={({ target }) => setEmail(target.value)}
           InputProps={{
             endAdornment: (
@@ -70,6 +109,7 @@ export default function Footer() {
                     textTransform: "none",
                   }}
                   disableElevation
+                  onClick={onGetPremiumReport}
                 >
                   Get premium report
                 </Button>
@@ -78,10 +118,18 @@ export default function Footer() {
             style: { padding: 0 },
           }}
         />
-        <Typography variant="body1" style={{ color: "white", marginTop: 5 }} align="right">
+        <Typography
+          variant="body1"
+          style={{ color: "white", marginTop: 5 }}
+          align="right"
+        >
           Explore app
         </Typography>
-        <Typography variant="body1" style={{ color: "white", marginTop: 5 }} align="right">
+        <Typography
+          variant="body1"
+          style={{ color: "white", marginTop: 5 }}
+          align="right"
+        >
           Contact us
         </Typography>
       </Grid>
--- sections/pre_live_home_sections/topBanner.tsx
+++ sections/pre_live_home_sections/topBanner.tsx
@@ -1,74 +1,113 @@
 "use client";
 import { useState } from "react";
-import { Grid, Typography, TextField, Button, useMediaQuery, InputAdornment } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  TextField,
+  Button,
+  useMediaQuery,
+  InputAdornment,
+} from "@mui/material";
 import Image from "next/image";
-import { useTheme } from '@mui/material/styles';
+import { useTheme } from "@mui/material/styles";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export default function TopBanner() {
+  const supabase = createClientComponentClient();
   const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState(false)
   const theme = useTheme();
-  const md = useMediaQuery(theme.breakpoints.down('md'));
+  const md = useMediaQuery(theme.breakpoints.down("md"));
+
+  const onGetPremiumReport = async () => {
+    setEmailError(false);
+    if(!email.match(/.+@.+/)){
+      setEmailError(true);
+      return
+    }
+    await supabase.from("email").insert([
+      {
+        email: email,
+      },
+    ]);
+    setEmail("")
+  };
   return (
     <Grid
       container
       spacing={2}
-    //   sx={{
-    //     backgroundImage: "url('/Banner.png')",
-    //     backgroundRepeat: "no-repeat",
-    //     backgroundPositionY: "100%",
-    //     backgroundPositionX: "50%",
-    //     backgroundSize: "60% 50%",
-    //     backgroundAttachment: "scroll"
-    //   }}
+      //   sx={{
+      //     backgroundImage: "url('/Banner.png')",
+      //     backgroundRepeat: "no-repeat",
+      //     backgroundPositionY: "100%",
+      //     backgroundPositionX: "50%",
+      //     backgroundSize: "60% 50%",
+      //     backgroundAttachment: "scroll"
+      //   }}
     >
-      <Grid item xs={ !md ? 6 : 12} sx={{ m: 5,}}>
-        <Typography variant="h2" component="h2" style={{ fontWeight: 900}}>
+      <Grid item xs={!md ? 6 : 12} sx={{ m: 5 }}>
+        <Typography variant="h2" component="h2" style={{ fontWeight: 900 }}>
           <span style={{ backgroundColor: "black", color: "white" }}>
             Empowering
-          </span><span> </span>
+          </span>
+          <span> </span>
           investors through AI powered insights.
         </Typography>
-        <Typography variant="body1" sx={{ mt: 1 }} style={{ fontWeight: 600}}>
+        <Typography variant="body1" sx={{ mt: 1 }} style={{ fontWeight: 600 }}>
           Eliminate hours wasted finding, cleaning, visualizing and transforming
           data using <br /> the power of AI. <br />
           Register and get a free premium report to your inbox.
         </Typography>
-        
+
         <Grid container spacing={0} sx={{ mt: 2 }}>
           <Grid item xs={7}>
             <TextField
               label="Email"
               variant="outlined"
-              sx={{ width: "35vw", p:0 }}
+              sx={{ width: "35vw", p: 0 }}
               value={email}
               size="small"
+              error={emailError}
+              helperText={emailError ? "Please enter a valid email.": ""}
               onChange={({ target }) => setEmail(target.value)}
-              InputProps={{endAdornment: <InputAdornment position="end"><Button
-                variant="contained"
-                style={{
-                  backgroundColor: "#73F3C4",
-                  color: "black",
-                  textTransform: "none",
-                  fontWeight: 900,
-                  
-                }}
-                disableElevation
-              >
-                Get free premium report
-              </Button>
-              </InputAdornment>,
-              style: {padding: 0}}}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Button
+                      variant="contained"
+                      style={{
+                        backgroundColor: "#73F3C4",
+                        color: "black",
+                        textTransform: "none",
+                        fontWeight: 900,
+                      }}
+                      disableElevation
+                      onClick={onGetPremiumReport}
+                    >
+                      Get free premium report
+                    </Button>
+                  </InputAdornment>
+                ),
+                style: { padding: 0 },
+              }}
             />
           </Grid>
         </Grid>
       </Grid>
-      <Grid item xs={9.5} sx={{ ml:'auto',mr: 'auto',mt: 12,p:0, position: "relative", display: {lg: 'block', xs: 'none'} , minHeight: "80vh"}}>
-        <Image
-          src="/Banner.png"
-          alt="banner"
-          fill
-          objectFit={'contain'}
-        />
+      <Grid
+        item
+        xs={9.5}
+        sx={{
+          ml: "auto",
+          mr: "auto",
+          mt: 12,
+          p: 0,
+          position: "relative",
+          display: { lg: "block", xs: "none" },
+          minHeight: "80vh",
+        }}
+      >
+        <Image src="/Banner.png" alt="banner" fill objectFit={"contain"} />
       </Grid>
     </Grid>
   );

14e7b5021e77b1228e637cc11b2e6cd87a4663a4
Aditya Singh <adity.347317@gmail.com>
2023-09-27 19:16:47 +0530

    Added backend logic for sig=ngle blog page

--- components/blogCards.tsx
+++ components/blogCards.tsx
@@ -1,12 +1,20 @@
 import { CardContent, Card, CardMedia, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 import moment from "moment";
+import { useState } from "react";
 
 export default function BlogCards(props: any) {
   const item = props.item;
   const { push } = useRouter();
   const imgUrl =
     "https://yjasfeanlannyjroczqf.supabase.co/storage/v1/object/public/blog-images/";
+  const [queryParams, setQueryParams] = useState({})
+
+  const onBlogClick = (props: any) => {
+    const queryParams = props.item
+    const query = new URLSearchParams(queryParams).toString();
+    push(`/blog/${item.id}?${query}`)
+  }
 
   return (
     <Card
@@ -18,18 +26,18 @@ export default function BlogCards(props: any) {
         borderWidth: 2.5,
         borderRadius: 10,
         cursor: "pointer",
-        minHeight: 450,
-        overflow: 'none'
+        minHeight: 430,
+        overflow: "none",
       }}
       sx={{ boxShadow: 5 }}
-      onClick={() => push(`/blog/${item.id}`)}
+      onClick={() => onBlogClick(props)}
     >
       <CardMedia
         component="img"
         height="150"
         image={imgUrl + item.thumbnail.replace(" ", "%20")}
         alt="Banner"
-        sx={{  objectFit: "fill", maxHeight: "35vh" }}
+        sx={{ objectFit: "fill", maxHeight: "33vh" }}
       />
       <CardContent>
         <Typography variant="body2" sx={{ fontWeight: 700, fontSize: "2rem" }}>
--- next.config.js
+++ next.config.js
@@ -9,6 +9,16 @@ const nextConfig = {
           },
         ]
       },
+      images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'yjasfeanlannyjroczqf.supabase.co',
+            port: '',
+            pathname: '/storage/v1/object/public/blog-images/**',
+          },
+        ],
+      },
 }
 
 module.exports = nextConfig

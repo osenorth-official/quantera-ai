d42c50f9e76629465ebb00a529b17a184d721ac2
Aditya Pratap Singh <53206666+adi16031@users.noreply.github.com>
2023-10-13 08:03:15 +0000

    Added style to card

--- components/blogCards.tsx
+++ components/blogCards.tsx
@@ -22,6 +22,16 @@ export default function BlogCards(props: any) {
       variant="outlined"
       className={styles.card} // Apply the CSS class for responsive styles
       onClick={onBlogClick}
+      style={{borderColor: "black",
+        borderWidth: "2px",
+        borderRadius: "10px",
+        cursor: "pointer",
+        minHeight: "400px",
+        overflow: "hidden",
+        width: "30vw",
+        margin: "20px",
+        boxShadow: "8px 8px 8px rgba(0, 0, 0, 0.202)"
+      }}
     >
       <CardMedia
         component="img"
